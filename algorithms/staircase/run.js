9a8050ef841ab9e09bac1b51b6172ba9bce64260
Long Do <me@dotronglong.com>
2016-08-19 23:31:45 +0700

    Remove result

--- algorithms/staircase/run.js
+++ algorithms/staircase/run.js
@@ -3,15 +3,4 @@ io.readInputLines(main);
 
 function main() {
     var n = parseInt(io.readLine());
-    for (var i = n - 1; i >= 0; i--) {
-        var str = "";
-        for (var j = 0; j < n; j++) {
-            if (j < i) {
-                str += " ";
-            } else {
-                str += "#";
-            }
-        }
-        console.log(str);
-    }
 }
