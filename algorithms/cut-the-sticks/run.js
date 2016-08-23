0be5609181c1376f3776cbd725a34e600af00aa7
Long Do <long.do@audiencemedia.com>
2016-08-23 16:56:30 +0700

    Add Cut The Sticks

---
+++ algorithms/cut-the-sticks/run.js
@@ -0,0 +1,8 @@
+var io = require("../../utils").io;
+io.readInputLines(main);
+
+function main() {
+    var n = parseInt(io.readLine());
+    var a = io.readLine().split(' ');
+    a = a.map(Number);
+}
