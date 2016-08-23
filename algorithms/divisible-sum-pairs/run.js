22711a54c2df0114438f9f219688d251879cb3f8
Long Do <long.do@audiencemedia.com>
2016-08-23 15:02:20 +0700

    Add Divisible Sum Pairs

---
+++ algorithms/divisible-sum-pairs/run.js
@@ -0,0 +1,9 @@
+var io = require("../../utils").io;
+io.readInputLines(main);
+
+function main() {
+    var args = io.readLine().split(" ");
+    var n = parseInt(args[0]),
+    	k = parseInt(args[1]);
+    var a = io.readLine().split(" ").map(Number);
+}
